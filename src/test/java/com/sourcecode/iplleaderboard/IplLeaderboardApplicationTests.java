21e08a9204f84af4aa4ecc0aa094d439205e516d
Jasmin Nasit <jasujnasit48@gmail.com>
2021-05-08 13:49:51 +0530

    starter project

---
+++ src/test/java/com/sourcecode/iplleaderboard/IplLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcecode.iplleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
