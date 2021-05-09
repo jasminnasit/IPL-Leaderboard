c51f5c669525de5d7ff90246818d23a93bb9f9d8
Jasmin Nasit <jasujnasit48@gmail.com>
2021-05-09 11:47:40 +0530

    added pojo and processor for match data processing

---
+++ src/main/java/com/sourcecode/iplleaderboard/data/MatchDataProcessor.java
@@ -0,0 +1,28 @@
+package com.sourcecode.iplleaderboard.data;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.sourcecode.iplleaderboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+
+        log.debug(match.toString());
+        return match;
+    }
+
+}
