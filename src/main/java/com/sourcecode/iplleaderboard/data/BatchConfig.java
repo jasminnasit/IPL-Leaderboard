7e90c66ee84907882255d26f68eb1e3c6770a7b9
Jasmin Nasit <jasujnasit48@gmail.com>
2021-05-13 22:43:35 +0530

    bug fix

--- src/main/java/com/sourcecode/iplleaderboard/data/BatchConfig.java
+++ src/main/java/com/sourcecode/iplleaderboard/data/BatchConfig.java
@@ -23,11 +23,11 @@ import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableBatchProcessing
-public class BatchConfig<JobCompletionNotificationListener> {
+public class BatchConfig {
 
-    private final String[] FIELD_NAMES = new String[] { "id", "city", "date,player_of_match", "venue,neutral_venue",
-            "team1", "team2", "toss_winner", "toss_decision", "winner", "result", "result_margin", "eliminator",
-            "method", "umpire1", "umpire2" };
+    private final String[] FIELD_NAMES = new String[] { "id", "city", "date", "player_of_match", "venue",
+            "neutral_venue", "team1", "team2", "toss_winner", "toss_decision", "winner", "result", "result_margin",
+            "eliminator", "method", "umpire1", "umpire2" };
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
