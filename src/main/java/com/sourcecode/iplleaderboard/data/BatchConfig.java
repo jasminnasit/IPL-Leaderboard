746fd4f6e5240517aefc84e7a98944c4736a3ad2
Jasmin Nasit <jasujnasit48@gmail.com>
2021-05-13 13:48:13 +0530

    created batch job to process data

--- src/main/java/com/sourcecode/iplleaderboard/data/BatchConfig.java
+++ src/main/java/com/sourcecode/iplleaderboard/data/BatchConfig.java
@@ -1,8 +1,18 @@
 package com.sourcecode.iplleaderboard.data;
 
+import javax.sql.DataSource;
+
+import com.sourcecode.iplleaderboard.model.Match;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -13,7 +23,7 @@ import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableBatchProcessing
-public class BatchConfig {
+public class BatchConfig<JobCompletionNotificationListener> {
 
     private final String[] FIELD_NAMES = new String[] { "id", "city", "date,player_of_match", "venue,neutral_venue",
             "team1", "team2", "toss_winner", "toss_decision", "winner", "result", "result_margin", "eliminator",
@@ -39,6 +49,27 @@ public class BatchConfig {
     @Bean
     public MatchDataProcessor processor() {
         return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Match>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO match (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, result_margin, umpire1, umpire2) "
+                        + "VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, :matchWinner, :result, :resultMargin, :umpire1, :umpire2)")
+                .dataSource(dataSource).build();
+    }
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory.get("importUserJob").incrementer(new RunIdIncrementer()).listener(listener).flow(step1)
+                .end().build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Match> writer) {
+        return stepBuilderFactory.get("step1").<MatchInput, Match>chunk(10).reader(reader()).processor(processor())
+                .writer(writer).build();
     }
 
 }
