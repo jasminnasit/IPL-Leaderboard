36cb48be2bbe6d7f3e4c63551bacb1026802efc4
jasminnasit <jasujnasit48@gmail.com>
2021-05-20 10:19:34 +0530

    created team page UI

---
+++ src/frontend/src/pages/TeamPage.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+
+
+export const TeamPage = () => {
+  return (
+    <div className="TeamPage">
+      <h1>Team Name</h1>
+      <h3>Latest Mathches</h3>
+    </div>
+  );
+}
+
