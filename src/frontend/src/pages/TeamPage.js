afccc6bf6065c650b0c86032e22511b30fc11117
jasminnasit <jasujnasit48@gmail.com>
2021-05-23 16:37:32 +0530

    added route for team

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../component/MatchDetailCard';
 import { MatchSmallCard } from '../component/MatchSmallCard';
 
@@ -6,27 +7,29 @@ import { MatchSmallCard } from '../component/MatchSmallCard';
 export const TeamPage = () => {
 
     const [team, setTeam] = useState({matches : []});
-
+    const { teamName } = useParams();
     useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch('http://localhost:8080/team/Delhi%20Capitals')
+                const response = await fetch(`http://localhost:8080/team/${teamName}`)
                 const data = await response.json();
                 setTeam(data);
             };
  
             fetchMatches();
         }, []
-    )
+    );
 
+    if (!team || !team.teamName) {
+        return (<h1> team not found </h1>);
+    }
+    return (
+        <div className="TeamPage">
+            <h1>{team.teamName}</h1>
+            <MatchDetailCard match={team.matches[0]} />
 
-  return (
-    <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
-      
-      { team.matches.slice(1).map(match => <MatchSmallCard match={match}/>) }
-    </div>
-  );
+            { team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+        </div>
+    );
 }
 
