665656f034392380189c76e69b618ffa5a406d48
jasminnasit <jasujnasit48@gmail.com>
2021-05-24 21:27:01 +0530

    add backend match API

--- src/main/java/com/sourcecode/iplleaderboard/controller/TeamController.java
+++ src/main/java/com/sourcecode/iplleaderboard/controller/TeamController.java
@@ -1,6 +1,11 @@
 package com.sourcecode.iplleaderboard.controller;
 
 import com.sourcecode.iplleaderboard.model.Team;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.sourcecode.iplleaderboard.model.Match;
 import com.sourcecode.iplleaderboard.repository.MatchRepository;
 import com.sourcecode.iplleaderboard.repository.TeamRepository;
 
@@ -8,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +36,12 @@ public class TeamController {
         Team team = teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return matchRepository.getMatchesByTeamBetweenDates(teamName,startDate,endDate);
     }
 }
